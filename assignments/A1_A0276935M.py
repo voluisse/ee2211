64712483714bafefa03354d36cfa27ad6677372b
Phoebe <65488865+voluisse@users.noreply.github.com>
2025-03-24 13:39:22 +0800

    Create A1_A0276935M.py

---
+++ assignments/A1_A0276935M.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+# Please replace "MatricNumber" with your actual matric number here and in the filename
+def A1_A0276935M(X, y):
+    """
+    Input type
+    :X type: numpy.ndarray
+    :y type: numpy.ndarray
+
+    Return type
+    :InvXTX type: numpy.ndarray
+    :w type: numpy.ndarray
+   
+    """
+
+    # your code goes here
+    X_transpose = np.transpose(X)
+    InvXTX = np.linalg.inv(X_transpose @ X)
+    w = InvXTX @ X_transpose @ y
+
+    # return in this order
+    return InvXTX, w
+
+
